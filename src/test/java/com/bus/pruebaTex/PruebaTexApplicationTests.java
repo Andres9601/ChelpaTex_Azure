87b360b85cbc5da7631c8dab031a82c72a739d36
Andres9601 <49967902+Andres9601@users.noreply.github.com>
2022-12-12 16:24:42 -0500

    Update PruebaTexApplicationTests.java

--- src/test/java/com/bus/pruebaTex/PruebaTexApplicationTests.java
+++ src/test/java/com/bus/pruebaTex/PruebaTexApplicationTests.java
@@ -1,13 +1,8 @@
 package com.bus.pruebaTex;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PruebaTexApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
