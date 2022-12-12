bd21abeff3a6d91079bd6617f16f50e52ec1dc81
Andres9601 <49967902+Andres9601@users.noreply.github.com>
2022-12-12 15:48:44 -0500

    Update ChelpaTexApplication.java

--- src/main/java/com/bus/chelpaTex/ChelpaTexApplication.java
+++ src/main/java/com/bus/chelpaTex/ChelpaTexApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.bus.chelpaTex.Entity","com.bus.chelpaTex.Controller","com.bus.chelpaTex.DTO","com.bus.chelpaTex.Repo","com.bus.chelpaTex.Service","com.bus.chelpaTex.ServiceImpl","com.bus.chelpaTex.Security"})
+@ComponentScan(basePackages = {"com.bus.chelpaTex.Entity","com.bus.chelpaTex.Controller","com.bus.chelpaTex.DTO","com.bus.chelpaTex.Repo","com.bus.chelpaTex.Service","com.bus.chelpaTex.ServiceImpl"})
 public class ChelpaTexApplication {
 
 	public static void main(String[] args) {
